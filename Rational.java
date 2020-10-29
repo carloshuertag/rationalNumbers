d5388a336d2e8d33cba3b6024c74c47b76fa0154
Carlos Huerta García <huerta2502@hotmail.com>
2020-10-29 09:15:32 -0600

    Here we go again

---
+++ Rational.java
@@ -0,0 +1,121 @@
+import java.util.Random;
+
+public class Rational {
+
+    private int numerator, denominator;
+
+    public Rational() {
+        Random random = new Random();
+        numerator = random.nextInt(9);
+        denominator = random.nextInt(9 - 1 + 1) + 1;
+    }
+
+    public Rational(int numerator, int denominator) {
+        this.numerator = numerator;
+        if(denominator == 0)
+            denominator = 1;
+        this.denominator = denominator;
+    }
+
+    public void setRational(int numerator, int denominator) {
+        this.numerator = numerator;
+        if(denominator == 0)
+            denominator = 1;
+        this.denominator = denominator;
+    }
+
+    private int lcm(int n1, int n2) {
+        int mcm = n1 > n2 ? n1 : n2;
+        while(mcm % n1 != 0 || mcm % n2 != 0)
+            mcm++;
+        return mcm;
+    }
+
+    private int gcd(int a, int b) {
+        if (a == 0)
+          return b;
+        if (b == 0)
+          return a;
+        if (a == b)
+            return a;
+        return a > b ? gcd(a - b, b) : gcd(a, b - a);
+    }
+
+    public void reduce() {
+        int gcd = gcd(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    public void add(Rational r) {
+        numerator = (lcm(denominator, r.denominator) / denominator) * numerator + (lcm(denominator, r.denominator) / r.denominator) * r.numerator;
+        denominator = lcm(denominator, r.denominator);
+    }
+
+    public Rational addition(Rational r) {
+        return new Rational((lcm(denominator, r.denominator) / denominator) * numerator + (lcm(denominator, r.denominator) / r.denominator) * r.numerator, lcm(denominator, r.denominator));
+    }
+
+    public Rational addition(Rational r1, Rational r2) {
+        return new Rational((lcm(r1.denominator, r2.denominator) / r1.denominator) * r1.numerator + (lcm(r1.denominator, r2.denominator) / r2.denominator) * r2.numerator, lcm(r1.denominator, r2.denominator));
+    }
+
+    public void sub(Rational r) {
+        numerator = (lcm(denominator, r.denominator) / denominator) * numerator - (lcm(denominator, r.denominator) / r.denominator) * r.numerator;
+        denominator = lcm(denominator, r.denominator);
+    }
+
+    public Rational subtraction(Rational r){
+        return new Rational((lcm(denominator, r.denominator) / denominator) * numerator - (lcm(denominator, r.denominator) / r.denominator) * r.numerator, lcm(denominator, r.denominator));
+    }
+
+    public Rational subtraction(Rational r1, Rational r2){
+        return new Rational((lcm(r1.denominator, r2.denominator) / r1.denominator) * r1.numerator - (lcm(r1.denominator, r2.denominator) / r2.denominator) * r2.numerator, lcm(r1.denominator, r2.denominator));
+    }
+
+    public void mult(Rational r){
+        numerator *= r.numerator;
+        denominator *= r.denominator;
+    }
+
+    public Rational multiplication(Rational r){
+        return new Rational(numerator * r.numerator, denominator * r.denominator);
+    }
+
+    public Rational multiplication(Rational r1, Rational r2){
+        return new Rational(r1.numerator * r2.numerator, r1.denominator * r2.denominator);
+    }
+
+    public void div(Rational r){
+        numerator *= r.denominator;
+        denominator *= r.numerator;
+    }
+
+    public Rational division(Rational r){
+        return new Rational(numerator * r.denominator, denominator * r.numerator);
+    }
+
+    public Rational division(Rational r1, Rational r2){
+        return new Rational(r1.numerator * r2.denominator, r1.denominator * r2.numerator);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Rational){
+            Rational r = (Rational) obj;
+            if(r.numerator == this.numerator && r.denominator == this.denominator)
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return numerator + "/" + denominator;
+    }
+
+    public void print(){
+        System.out.print("{" + this + "}\n");
+    }
+
+}
---
+++ UserRational.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class UserRational {
+    public static void main(String[] args){
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int op, numerator, denominator;
+        char next = 'S';
+        do {
+            Rational rational1 = new Rational(), rational2 = new Rational(), result = null;
+            System.out.println("\nOperaciones con números racionales.\n\nSuma\nResta\nDivisión\nMultiplicación\n");
+            rational1.reduce();
+            rational2.reduce();
+            System.out.println("\nDos números racionales aleatorios: "+ rational1 + ", " + rational2);
+            op = random.nextInt(4 - 1 + 1) + 1;
+            switch(op) {
+                case 1:
+                    result = rational1.addition(rational2);
+                    System.out.println("Suma: " + rational1 + " + " + rational2 + " = ");
+                    break;
+                case 2:
+                    result = rational1.subtraction(rational2);
+                    System.out.println("Resta: " + rational1 + " - " + rational2 + " = ");
+                    break;
+                case 3:
+                    result = rational1.division(rational2);
+                    System.out.println("División " + rational1 + " / " + rational2 + " = ");
+                    break;
+                case 4:
+                    result = rational1.multiplication(rational2);
+                    System.out.println("Multiplicación " + rational1 + " * " + rational2 + " = ");
+                    break;
+                default:
+                    break;
+            }
+            result.reduce();
+            do {
+                System.out.println("\nIngrese el resultado obtenido reducido al máximo:\nNumerador: ");
+                numerator = scanner.nextInt();
+                System.out.println("Denominador: ");
+                denominator = scanner.nextInt();
+                rational1 = new Rational(numerator, denominator);
+                if(rational1.equals(result)){
+                    System.out.println("El resultado ingresado es correcto, ¡felicidades!\n\n¿Desea seguir practicando? (S/N)");
+                    next = scanner.next().charAt(0);
+                } 
+                else {
+                    System.out.println("El resultado ingresado es incorrecto, \nrevisa con detenimiento los pasos intermedios para la operación e intenta de nuevo.");
+                    next = 'N';
+                }
+            } while(next == 'N');
+        } while(next == 'S' || next == 's');
+        scanner.close();
+    }
+}
